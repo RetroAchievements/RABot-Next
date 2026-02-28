e2b4ef63c5504fb85d701d5659e0af286e706509
monkey-bug <136386334+monkey-bug@users.noreply.github.com>
2026-02-27 20:17:33 -0600

    refactor(AchievementUnlocks): add retry limit and inline waiting logic

--- src/services/achievement-unlocks.service.ts
+++ src/services/achievement-unlocks.service.ts
@@ -2,31 +2,15 @@ import { buildAuthorization, getAchievementUnlocks } from "@retroachievements/ap
 
 import { RA_WEB_API_KEY } from "../config/constants";
 
-const api = new (class {
-  #sleepBase = 200;
-  #failures = 0;
-
-  async wait() {
-    return new Promise<void>((resolve) =>
-      setTimeout(() => resolve(), this.#sleepBase * Math.pow(2, this.#failures++)),
-    );
-  }
-
-  reset() {
-    this.#failures = 0;
-  }
-})();
-
 export const PAGE_SIZE = 500;
 
 export class AchievementUnlocksService {
   static async getAllAchievementUnlocks(achievementId: number): Promise<string[] | null> {
     const auth = buildAuthorization({ username: "RABot", webApiKey: RA_WEB_API_KEY });
 
-    // retry after waiting upon recieving a 429 response, fail on any other error response
+    // retry after waiting up to 5 times upon receiving a 429 response, fail on any other error response
     const fetchUnlocks = async (offset: number) => {
-      api.reset();
-      for (;;) {
+      for (let tries = 0; tries < 5; tries++) {
         try {
           return await getAchievementUnlocks(auth, {
             achievementId,
@@ -34,8 +18,8 @@ export class AchievementUnlocksService {
             count: PAGE_SIZE,
           });
         } catch (error) {
-          if (error instanceof Error && error.message.search("429") !== -1) {
-            await api.wait();
+          if (error instanceof Error && error.message.includes("429")) {
+            await new Promise<void>((resolve) => setTimeout(() => resolve(), Math.pow(2, tries) * 200));
             continue;
           } else {
             return null;
